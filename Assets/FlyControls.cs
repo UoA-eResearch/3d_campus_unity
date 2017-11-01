ea5739e95407902dc31312e58e03ffcd9c25a1b9
Nick Young <nick.young@auckland.ac.nz>
2017-11-01 17:43:39 +1300

    init

---
+++ Assets/FlyControls.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class FlyControls : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+		float vertical = CrossPlatformInputManager.GetAxis("Vertical");
+		Debug.Log(horizontal);
+		Debug.Log(vertical);
+	}
+}
