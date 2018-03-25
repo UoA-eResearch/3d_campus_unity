dc7066976ebb753de3b6e942c9b2b2dd1b2a09e7
Nick Young <nick.young@auckland.ac.nz>
2018-03-26 12:37:39 +1300

    add snapto script, make photosphere bigger

---
+++ Assets/SnapTo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapTo : MonoBehaviour {
+
+	private float startTime;
+	public GameObject player;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		startTime = Time.time;
+	}
+
+	private void Update()
+	{
+		var s = Time.time - startTime;
+		if (s < 1)
+		{
+			player.transform.position = Vector3.Lerp(player.transform.position, transform.position, Time.time - startTime);
+		}
+	}
+}
