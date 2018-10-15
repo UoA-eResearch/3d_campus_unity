e8429e0360ae53a333ff98828959099f06d7fbd2
Nick Young <nyou045@UoA.auckland.ac.nz>
2018-10-15 13:09:38 +1300

    Scale labels based on distance from camera, tweak label style

--- Assets/FaceCamera.cs
+++ Assets/FaceCamera.cs
@@ -3,9 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FaceCamera : MonoBehaviour {
+
+	public bool scaleWithDistance;
+	private float scaleMin = 0;
+	private float scaleFactor = .1f;
+	private float scaleMax = 2;
 	
 	// Update is called once per frame
 	void Update () {
 		transform.LookAt(Camera.main.transform);
+		if (scaleWithDistance)
+		{
+			float dist = Vector3.Distance(transform.position, Camera.main.transform.position);
+			dist *= scaleFactor;
+			dist = Mathf.Clamp(dist, scaleMin, scaleMax);
+			transform.localScale = Vector3.one * dist;
+		}
 	}
 }
