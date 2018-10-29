c8e90eb9d97930db7e6643f8f65680846b086005
Nick Young <nyou045@UoA.auckland.ac.nz>
2018-10-30 12:10:03 +1300

    label / button styling alt text change to black scale label boxes to text size scale stalks with camera distance too center yt play icon, whiteout yt bg downscale buttons 25%. make button text blue

--- Assets/FaceCamera.cs
+++ Assets/FaceCamera.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class FaceCamera : MonoBehaviour {
 
+	public bool faceCamera = true;
 	public bool scaleWithDistance;
+	public bool scalexz;
 	private float scaleMin = 0;
 	private float scaleFactor = .04f;
 	private float scaleMax = 1;
 	
 	// Update is called once per frame
 	void Update () {
-		transform.LookAt(Camera.main.transform);
+		if (faceCamera)
+		{
+			transform.LookAt(Camera.main.transform);
+		}
 		if (scaleWithDistance)
 		{
 			float dist = Vector3.Distance(transform.position, Camera.main.transform.position);
 			dist *= scaleFactor;
 			dist = Mathf.Clamp(dist, scaleMin, scaleMax);
-			transform.localScale = Vector3.one * dist;
+			var scale = Vector3.one * dist;
+			if (scalexz)
+			{
+				scale.y = transform.localScale.y;
+			}
+			transform.localScale = scale;
 		}
 	}
 }
